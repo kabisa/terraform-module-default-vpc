4ddfd641cc8839e39bceb72104ff77ea121a5028
Luk van den Borne <luk.van.den.borne@kabisa.nl>
2019-09-17 16:23:07 +0200

    Suffix the prefix with a dash

--- s3_flow_logs.tf
+++ s3_flow_logs.tf
@@ -1,7 +1,7 @@
 resource "aws_s3_bucket" "flow_logs" {
   count         = "${var.enable_vpc_flow_logs}"
   acl           = "private"
-  bucket_prefix = "vpc-flow-logs-${var.account_id}"
+  bucket_prefix = "vpc-flow-logs-${var.account_id}-"
 
   server_side_encryption_configuration {
     rule {
